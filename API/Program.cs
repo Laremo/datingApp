a735267c7f882061ecee295b759ae394f6a908a0
laremo <memoo.lara12306@hotmail.com>
2025-08-28 07:48:04 -0600

    parcial01: Added first migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+builder.Services.AddDbContext<AppDataContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
 
 var app = builder.Build();
 
