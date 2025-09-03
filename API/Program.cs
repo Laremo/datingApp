b078da748e286928e6a8560180c4d33a6c976bb9
laremo <memoo.lara12306@hotmail.com>
2025-09-03 07:56:08 -0600

    parcial01: injects http to angular app, configured cors policy

--- API/Program.cs
+++ API/Program.cs
@@ -8,9 +8,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 
+// CORS Policy.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowClient",
+       policy =>
+       {
+           policy.WithOrigins("http://localhost:4200")
+                 .AllowAnyHeader()
+                 .AllowAnyMethod();
+       });
+});
+
 builder.Services.AddDbContext<AppDataContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
 
 var app = builder.Build();
+
+app.UseCors("AllowClient");
 
 app.UseAuthorization();
 
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  private htttp = inject(HttpClient);
+  protected readonly title = signal('Dating App');
+  ngOnInit(): void {
+    this.htttp.get('https://localhost:7121/api/members').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.error(error),
+      complete: () => {
+        console.log('completed!');
+      },
+    });
+  }
 }
