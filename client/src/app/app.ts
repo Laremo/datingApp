4b00817001c5f9d52cb4ec25dc5de970fbc42617
laremo <memoo.lara12306@hotmail.com>
2025-09-07 19:14:47 -0600

    parcial01: display fetched users

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,23 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App implements OnInit {
   private htttp = inject(HttpClient);
   protected readonly title = signal('Dating App');
-  ngOnInit(): void {
-    this.htttp.get('https://localhost:7121/api/members').subscribe({
-      next: (response) => console.log(response),
-      error: (error) => console.error(error),
-      complete: () => {
-        console.log('completed!');
-      },
-    });
+  protected members = signal<any>([]);
+  async ngOnInit(): Promise<void> {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers() : Promise<object> {
+    try {
+      return lastValueFrom(this.htttp.get('https://localhost:7121/api/members'));
+    } catch (error: unknown) {
+      console.log(error);
+      throw error;
+    }
   }
 }
