7dff1b7ccaf5ebf7f7eaa675b5051853a6ff9b87
laremo <memoo.lara12306@hotmail.com>
2025-09-30 07:47:28 -0600

    parcial02: add routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { MemberList } from '../features/members/member-list/member-list';
+import { MemberDetail } from '../features/members/member-detail/member-detail';
+import { Messages } from '../features/messages/messages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'members',
+    component: MemberList,
+  },
+  {
+    path: 'members/{id}',
+    component: MemberDetail,
+  },
+  {
+    path: 'lists',
+    component: MemberList,
+  },
+  {
+    path: 'messages',
+    component: Messages,
+  },
+  {
+    path: '**',
+    component: Home,
+  },
+];
