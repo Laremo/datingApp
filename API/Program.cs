5b3bbae77669e00d22b4516b1ecdd2e73bc1e2b0
laremo <memoo.lara12306@hotmail.com>
2025-10-06 07:52:22 -0600

    parcial02: Adds exception middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using API.Data;
 using API.Interfaces;
+using API.Middlewares;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -32,7 +33,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
     };
 });
 var app = builder.Build();
-
+// Middlewares van antes de todo(? 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader()
 .AllowAnyMethod()
 .WithOrigins(
