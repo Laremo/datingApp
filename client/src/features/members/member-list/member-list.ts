ed688d2d3269df4f3e22966621bcdba4b33652c3
laremo <memoo.lara12306@hotmail.com>
2025-10-23 07:36:10 -0600

    parcial03: add member list

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MembersService } from '../../../core/services/members-service';
+import { Member } from '../../../types/Member';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-member-list',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './member-list.html',
-  styleUrl: './member-list.css'
+  styleUrl: './member-list.css',
 })
 export class MemberList {
+  private membersService = inject(MembersService);
 
+  protected members$: Observable<Member[]>;
+
+  constructor() {
+    this.members$ = this.membersService.getMembers();
+  }
 }
