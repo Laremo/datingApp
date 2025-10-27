a4fe7b14c3d635dbb5071fdc5ef1dc52bfb771b0
laremo <memoo.lara12306@hotmail.com>
2025-10-27 07:19:12 -0600

    parcial03: add member card styles

--- client/src/features/members/member-card/member-card.ts
+++ client/src/features/members/member-card/member-card.ts
@@ -1,9 +1,9 @@
 import { Component, input } from '@angular/core';
 import { Member } from '../../../types/Member';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-member-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './member-card.html',
   styleUrl: './member-card.css',
 })
