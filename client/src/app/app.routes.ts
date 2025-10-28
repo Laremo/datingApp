5b58c56c68de0ed7cd3c74e95ba0bdae394e949f
laremo <memoo.lara12306@hotmail.com>
2025-10-28 07:48:08 -0600

    parcial03: add member detail child components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,6 +7,9 @@ import { authGuard } from '../core/guard/auth-guard';
 import { TestErrors } from '../features/test-errors/test-errors';
 import { NotFound } from '../shared/errors/not-found/not-found';
 import { ServerError } from '../shared/errors/server-error/server-error';
+import { MemberProfile } from '../features/member-profile/member-profile';
+import { MemberPhotos } from '../features/member-photos/member-photos';
+import { MemberMessages } from '../features/member-messages/member-messages';
 export const routes: Routes = [
   {
     path: '',
@@ -24,6 +27,12 @@ export const routes: Routes = [
       {
         path: 'members/:id',
         component: MemberDetail,
+        children: [
+          { path: '', redirectTo: 'profile', pathMatch: 'full' },
+          { path: 'profile', component: MemberProfile, title: 'Profile' },
+          { path: 'photos', component: MemberPhotos, title: 'Photos' },
+          { path: 'messages', component: MemberMessages, title: 'Messages' },
+        ],
       },
       {
         path: 'lists',
--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,13 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../../core/services/members-service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Member } from '../../../types/Member';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-member-detail',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive],
+  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css',
 })
