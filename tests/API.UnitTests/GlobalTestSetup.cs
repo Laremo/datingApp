a356df38957cbc2b61f49b050379323a24445e63
laremo <memoo.lara12306@hotmail.com>
2025-11-06 00:18:38 -0600

    parcial03: add first test

---
+++ tests/API.UnitTests/GlobalTestSetup.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.UnitTests;
+
+[SetUpFixture]
+public class GlobalTestSetup
+{
+   
+
+    public static AppDataContext AppDataContext { get; private set; }
+
+    [OneTimeSetUp]
+    public async Task Setup()
+    {
+        DbContextOptions<AppDataContext> options = new DbContextOptionsBuilder<AppDataContext>()
+        .UseSqlite("Data source=dating.db").Options;
+
+        AppDataContext = new AppDataContext(options);
+
+        await AppDataContext.Database.MigrateAsync();
+
+    }
+
+    [OneTimeTearDown]
+    public async Task TearDown()
+    {
+        await AppDataContext.DisposeAsync();
+    }
+
+}
