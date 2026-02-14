26e37a65733bc00f1c8b76105bb3a3b70110005b
Laremo <memoo.lara12306@hotmail.com>
2026-02-13 19:09:36 -0600

    feat: enhance member edito form

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -7,7 +7,6 @@ import {
   RouterLinkActive,
   RouterOutlet,
 } from '@angular/router';
-import { Member } from '../../../types/member';
 import { filter } from 'rxjs';
 import { AgePipe } from '../../../core/pipes/age-pipe';
 import { AccountService } from '../../../core/services/account-service';
@@ -23,17 +22,13 @@ export class MemberDetail implements OnInit {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private accountService = inject(AccountService);
-  protected membersService = inject(MembersService)
-  protected member = signal<Member | undefined>(undefined);
+  protected membersService = inject(MembersService);
   protected title = signal<string | undefined>('Profile');
   protected isCurrentUser = computed(() => {
     return this.accountService.currentUser()?.id === this.route.snapshot.paramMap.get('id');
   });
 
   ngOnInit(): void {
-    this.route.data.subscribe({
-      next: (data) => this.member.set(data['member']),
-    });
     this.title.set(this.route.firstChild?.snapshot?.title);
 
     this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe({
