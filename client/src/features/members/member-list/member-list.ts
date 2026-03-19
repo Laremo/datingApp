0d89e52a187b6cd1e7639bed020f6114650560e5
Laremo <memoo.lara12306@hotmail.com>
2026-03-18 19:38:33 -0600

    feat: add signal to avoid reloading paginator

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,30 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MembersService } from '../../../core/services/members-service';
 import { Member } from '../../../types/member';
-import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
 import { MemberCard } from '../member-card/member-card';
 import { PaginationResult } from '../../../types/PaginationMetadata';
 import { Paginator } from '../../../shared/paginator/paginator';
 
 @Component({
   selector: 'app-member-list',
-  imports: [AsyncPipe, MemberCard, Paginator],
+  imports: [MemberCard, Paginator],
   templateUrl: './member-list.html',
   styleUrl: './member-list.css',
 })
-export class MemberList {
+export class MemberList implements OnInit {
   private membersService = inject(MembersService);
-  protected paginatedMembers$?: Observable<PaginationResult<Member>>;
+  protected paginatedMembers = signal<PaginationResult<Member> | null>(null);
   pageNumber = 1;
   pageSize = 5;
-  
-  constructor() {
+
+  ngOnInit(): void {
     this.loadMembers();
   }
-
   loadMembers() {
-    this.paginatedMembers$ = this.membersService.getMembers(this.pageNumber, this.pageSize);
+    this.membersService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: (result) => {
+        this.paginatedMembers.set(result);
+      },
+    });
   }
 
   onPageChange(event: { pageNumber: number; pageSize: number }) {
