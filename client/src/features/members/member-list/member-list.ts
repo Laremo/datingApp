8ed96db604909613bb0695b3926a3437aba192d9
Laremo <memoo.lara12306@hotmail.com>
2026-03-26 16:40:33 -0600

    feat: adds filter modal

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,17 +1,19 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { MembersService } from '../../../core/services/members-service';
 import { Member, MemberParams } from '../../../types/member';
 import { MemberCard } from '../member-card/member-card';
 import { PaginationResult } from '../../../types/PaginationMetadata';
 import { Paginator } from '../../../shared/paginator/paginator';
+import { FilterModal } from '../filter-modal/filter-modal';
 
 @Component({
   selector: 'app-member-list',
-  imports: [MemberCard, Paginator],
+  imports: [MemberCard, Paginator, FilterModal],
   templateUrl: './member-list.html',
   styleUrl: './member-list.css',
 })
 export class MemberList implements OnInit {
+  @ViewChild('filterModal') modal!: FilterModal;
   private membersService = inject(MembersService);
   protected paginatedMembers = signal<PaginationResult<Member> | null>(null);
   pageNumber = 1;
@@ -32,6 +34,23 @@ export class MemberList implements OnInit {
   onPageChange(event: { pageNumber: number; pageSize: number }) {
     this.memberParams.pageNumber = event.pageNumber;
     this.memberParams.pageSize = event.pageSize;
+    this.loadMembers();
+  }
+
+  openModal() {
+    this.modal.open();
+  }
+
+  onClose() {
+    console.log('Modal closed');
+  }
+
+  onFilterChange(data: MemberParams) {
+    console.log('Modal submitted data: ', data);
+  }
+
+  resetFilters() {
+    this.memberParams = new MemberParams();
     this.loadMembers();
   }
 }
